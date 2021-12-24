97c74c6b0193e959e0d17f26cd10752e181cdcb5
gaollard <gaollard@foxmail.com>
2021-12-24 16:05:00 +0800

    t

--- packages/f6/gulpfile.js
+++ packages/f6/gulpfile.js
@@ -1,27 +1,75 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
 const path = require("path");
-const postcss = require("gulp-postcss");
 const babel = require("gulp-babel");
 const ts = require("gulp-typescript");
+const del = require('del')
+const tsconfig = require("./tsconfig.json");
+var tsProject = ts.createProject("tsconfig.json");
 
-function buildStyle() {
+const ignores = [
+  "**/demos/**/*",
+  "**/demo/**/*",
+  "**/page/**/*",
+  "**/tests/**/*",
+]
+
+// clean lib
+function clean() {
+  return del('./lib/**')
+}
+
+// build es module
+function buildES() {
+  const tsProject = ts({
+    ...tsconfig.compilerOptions,
+    module: "ESNext",
+  });
   return gulp
-    .src(["./packages/**/*.less"], {
-      // base: "./packages/",
-      ignore: ["**/demos/**/*", "**/tests/**/*"],
+    .src(["packages/**/*.{ts,tsx}"], {
+      ignore: ignores,
     })
+    .pipe(tsProject)
+    .pipe(babel())
+    .pipe(gulp.dest("lib/es/"));
+}
+
+// build commonjs module
+function buildCJS() {
+  return gulp
+    .src(["lib/es/**/*.js"])
     .pipe(
-      less({
-        paths: [path.join(__dirname, "packages")],
-        relativeUrls: true,
+      babel({
+        plugins: ["@babel/plugin-transform-modules-commonjs"],
       })
     )
-    .pipe(gulp.dest("./lib"))
-    // .pipe(gulp.dest("./lib/cjs"));
+    .pipe(gulp.dest("lib/cjs/"));
+}
+
+function buildDeclaration() {
+  const tsProject = ts({
+    // ...tsconfig.compilerOptions,
+    "declaration": true,
+    esModuleInterop: true,
+    "emitDeclarationOnly": true,
+    "jsx": "react-jsx",
+    declaration: true,
+    emitDeclarationOnly: true
+  })
+  return gulp
+    .src('packages/**/*.{ts,tsx}')
+    .pipe(tsProject)
+    .pipe(gulp.dest('lib'))
+    // .pipe(gulp.dest('./lib/cjs/'))
+// return tsProject.src()
+//   .pipe(tsProject())
+//   .pipe(gulp.dest("dist"));
 }
 
 
 exports.default = gulp.series(
-  gulp.parallel(buildStyle),
-)
+  buildDeclaration
+  // clean,
+  // buildES,
+  // gulp.parallel(buildCJS, buildDeclaration),
+);
